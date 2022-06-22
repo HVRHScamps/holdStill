e662c986ca887dae689827490efe51294e647b2d
lydia <>
2022-06-22 11:45:16 -0400

    yay

--- holdStill.py
+++ holdStill.py
@@ -1,6 +1,8 @@
 import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
 first = True
+
+p = 0.05
 def main(robot: libhousy.robot):
     global first
     if first:
@@ -9,16 +11,25 @@ def main(robot: libhousy.robot):
         first = False
 
     if robot.rDriveEncoder.Get() > 2:
-        robot.rDrive.Set(-.5)
+        error = 0.0 - robot.rDriveEncoder.Get()
+        speed = error * p
+        robot.rDrive.Set(speed)
     elif robot.rDriveEncoder.Get() <-2:
-       robot.rDrive.Set(.5) 
+        error = 0.0 - robot.rDriveEncoder.Get()
+        speed = error * p
+        robot.rDrive.Set(speed) 
     else:
        robot.rDrive.Set(0)
 
     if robot.lDriveEncoder.Get() > 2:
         robot.lDrive.Set(-.5)
+        error = 0.0 - robot.lDriveEncoder.Get()
+        speed = error * p
+        robot.lDrive.Set(speed)
     elif robot.lDriveEncoder.Get() < -2:
-        robot.lDrive.Set(.5)
+        error = 0.0 - robot.lDriveEncoder.Get()
+        speed = error * p
+        robot.lDrive.Set(speed)
     else:
         robot.lDrive.Set(0)
 
