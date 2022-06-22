399bfc1cbb1ce982e7894a0d7ccdbf663ce021c1
lydia <>
2022-06-22 11:00:29 -0400

    add stuffffff

--- holdStill.py
+++ holdStill.py
@@ -1,6 +1,26 @@
 import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
+first = True
 def main(robot: libhousy.robot):
-    #Here is where your recurring code will go
-    print("Hello World!")
+    if first:
+        robot.rDriveEncoder.Reset()
+        robot.lDriveEncoder.Reset()
+        first = False
+
+    if robot.rDriveEncoder.Get() >4:
+        robot.rDrive.set(-.5)
+    elif robot.rDriveEncoder.Get() <-4:
+       robot.rDrive.set(.5) 
+    else:
+       robot.rDrive.set(0)
+
+    if robot.lDriveEncoder.Get() > 4:
+        robot.lDrive.set(-.5)
+    elif robot.lDriveEncoder.Get() < -4:
+        robot.lDrive.set(.5)
+    else:
+        robot.lDrive.set(0)
+
     return libhousy.DONE
+    
+    
