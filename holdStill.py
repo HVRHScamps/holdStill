44817bbcf61f06d2ec098784c1849b2184d2ad02
cannotilever <51347140+cannotilever@users.noreply.github.com>
2022-06-26 11:11:48 -0400

    reset for week 2

--- holdStill.py
+++ holdStill.py
@@ -1,38 +1,9 @@
+# inclue code to help us talk to the robot
 import libhousy
-#You can define helper functions here, make sure to but them *above* the main function
-first = True
 
-p = 0.05
 def main(robot: libhousy.robot):
-    global first
-    if first:
-        robot.rDriveEncoder.Reset()
-        robot.lDriveEncoder.Reset()
-        first = False
-
-    if robot.rDriveEncoder.Get() > 2:
-        error = 0.0 - robot.rDriveEncoder.Get()
-        speed = error * p
-        robot.rDrive.Set(speed)
-    elif robot.rDriveEncoder.Get() <-2:
-        error = 0.0 - robot.rDriveEncoder.Get()
-        speed = error * p
-        robot.rDrive.Set(speed) 
-    else:
-       robot.rDrive.Set(0)
-
-    if robot.lDriveEncoder.Get() > 2:
-        robot.lDrive.Set(-.5)
-        error = 0.0 - robot.lDriveEncoder.Get()
-        speed = error * p
-        robot.lDrive.Set(speed)
-    elif robot.lDriveEncoder.Get() < -2:
-        error = 0.0 - robot.lDriveEncoder.Get()
-        speed = error * p
-        robot.lDrive.Set(speed)
-    else:
-        robot.lDrive.Set(0)
-
-    #return libhousy.DONE
+    # Here is where your recurring code will go
+    print("Hello World!")
     
-    
+    # After everything is done, we tell the main program to stop us
+    return libhousy.DONE
