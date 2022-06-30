bd1bc0f6db80aa10778934a0979989272a890ccf
Whackyman <dieselmntbiker69@outlook.com>
2022-06-30 09:35:04 -0400

    hold still

--- holdStill.py
+++ holdStill.py
@@ -4,19 +4,19 @@ import libhousy
 def main(robot: libhousy.robot):
     # Here is where your recurring code will go
     if robot.lDriveEncoder.Get() >0:
-        robot.lDrive.Set(-0.3)
+        robot.lDrive.Set(0)
         
     elif robot.lDriveEncoder.Get() <0:
-        robot.lDrive.Set(0.3)
+        robot.lDrive.Set(0)
         
     else: 
         robot.lDrive.Set(0)
 
     if robot.rDriveEncoder.Get() >0:
-        robot.rDrive.Set(-0.3)
+        robot.rDrive.Set(0)
 
     elif robot.rDriveEncoder.Get() <0:
-        robot.rDrive.Set(0.3)
+        robot.rDrive.Set(0)
     else:
         robot.rDrive.Set(0)
     
