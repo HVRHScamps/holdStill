df7acb6f0d8aa8e6411a373a39a0ae2a334e1daa
Logan <loganpadelli@gmail.com>
2022-06-30 09:49:16 -0400

    Yes

--- holdStill.py
+++ holdStill.py
@@ -1,9 +1,34 @@
 # inclue code to help us talk to the robot
 import libhousy
+first = True
 
+p = 0.05
 def main(robot: libhousy.robot):
-    # Here is where your recurring code will go
-    print("Hello World!")
-    
-    # After everything is done, we tell the main program to stop us
-    return libhousy.DONE
+    global first
+    if first:
+        robot.rDriveEncoder.Reset()
+        robot.lDriveEncoder.Reset()
+        first = False 
+
+    if robot.rDriveEncoder.Get() > 2:
+        error = 0.0 - robot.rDriveEncoder.Get()
+        speed = error * p 
+        robot.rDrive.Set(speed)
+    elif robot.rDriveEncoder.Get() < -2:
+        error = 0.0 - robot.rDriveEncoder.Get()
+        speed = error * p 
+        robot.rDrive.Set(speed)
+    else: 
+        robot.rDrive.Set(0)
+
+    if robot.lDriveEncoder.Get() > 2:
+        robot.lDrive.Set(-.5)
+        error = 0.0 - robot.lDriveEncoder.Get()
+        speed = error * p
+        robot.lDrive.Set(speed)
+    elif robot.lDriveEncoder.Get() < -2:
+        error = 0.0 - robot.lDriveEncoder.Get()
+        speed = error * p
+        robot.lDrive.Set(speed)
+    else:
+        robot.lDrive.Set(0)
